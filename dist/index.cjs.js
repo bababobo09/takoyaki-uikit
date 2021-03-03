51e9a0fbab20c9842ba67b686e277b235111a161
Takoyaki Admin <takoyakidefi@gmail.com>
2021-03-03 14:32:35 +0700

    update color

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2710,15 +2710,15 @@ var brandColors = {
   binance: "#F0B90B",
 };
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), {
-  background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#f2f2f2", tertiary: "#EFF4F5", text: '#D1601D', textDisabled: '#998c83', textSubtle: '#D76D29', borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
-    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+  background: "#FAF9FA", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#f2f2f2", tertiary: "#EFF4F5", text: '#242424', textDisabled: '#998c83', textSubtle: '#424242', borderColor: "#E9EAEB", card: "#FFFFFF", gradients: {
+    bubblegum: "linear-gradient(139.73deg, #fff9e6 0%, #fff8f2 100%)",
   }
 });
 var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), {
   secondary: "#DD7A35", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#58565c",
   // primaryDark: "#0098A1",
   tertiary: "#353547", text: "#f7f7f7", textDisabled: "#666171", textSubtle: "#d2d2d4", borderColor: "#615f66", card: "#27262c", gradients: {
-    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+    bubblegum: "linear-gradient(139.73deg, #5c4a31 0%, #543e2a 100%)",
   }
 });
 
