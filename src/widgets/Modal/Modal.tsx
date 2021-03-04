fb72c53bf940d39911c8d3be3ec749d37b6a3557
Takoyaki Admin <takoyakidefi@gmail.com>
2021-03-04 19:09:14 +0700

    update modal shadow color

--- src/widgets/Modal/Modal.tsx
+++ src/widgets/Modal/Modal.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import Heading from "../../components/Heading/Heading";
 import Flex from "../../components/Flex/Flex";
-import { ArrowBackIcon, CloseIcon } from "../../components/Svg";
-import { IconButton } from "../../components/Button";
-import { InjectedProps } from "./types";
+import {ArrowBackIcon, CloseIcon} from "../../components/Svg";
+import {IconButton} from "../../components/Button";
+import {InjectedProps} from "./types";
 
 interface Props extends InjectedProps {
   title: string;
@@ -14,14 +14,14 @@ interface Props extends InjectedProps {
 }
 
 const StyledModal = styled.div`
-  background: ${({ theme }) => theme.modal.background};
-  box-shadow: 0px 20px 36px -8px rgba(14, 14, 44, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.05);
-  border: 1px solid ${({ theme }) => theme.colors.borderColor};
+  background: ${({theme}) => theme.modal.background};
+  box-shadow: 0px 20px 36px -8px rgba(46, 46, 46, 0.1), 0px 1px 1px rgba(0, 0, 0, 0.05);
+  border: 1px solid ${({theme}) => theme.colors.borderColor};
   border-radius: 32px;
   width: 100%;
-  z-index: ${({ theme }) => theme.zIndices.modal};
+  z-index: ${({theme}) => theme.zIndices.modal};
   overflow-y: auto;
-  ${({ theme }) => theme.mediaQueries.xs} {
+  ${({theme}) => theme.mediaQueries.xs} {
     width: auto;
     min-width: 360px;
     max-width: 100%;
